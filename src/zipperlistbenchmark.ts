e624be7781c30782cc37615e13a0f2c91e7fc0dc
Lakin Wecker <lakin.wecker@gmail.com>
2021-11-12 14:10:23 -0700

    New depdendencies and benchmarks from last time

--- src/zipperlistbenchmark.ts
+++ src/zipperlistbenchmark.ts
@@ -1,15 +1,19 @@
 /* benchmark.js */
 import b from "benny";
-import { ZipperList, map } from "./zipperlist";
+import { ZipperList, map, mapOld } from "./zipperlist";
 
-const z = ZipperList([1, 2], 3, [4, 5]);
-const l = [1, 2, 3, 4, 5];
+const l = Array.from(new Array(1000000)).map((_, i) => i);
+const z = ZipperList([], 3, l);
 
 b.suite(
   "Example",
 
   b.add("Map ZipperList", () => {
     map((x: number) => x * x)(z);
+  }),
+
+  b.add("Map ZipperList Old", () => {
+    mapOld((x: number) => x * x)(z);
   }),
 
   b.add("Map Raw List", () => {
